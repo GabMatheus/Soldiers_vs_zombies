472fede34f78bd8744afcf22cb2e036462814a23
GabMatheus <gab_matheus@hotmail.com>
2025-03-16 15:49:08 -0300

    Creating assets and Start Menu

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+#C
+COLOR_GOLD = (212, 160, 28)
+COLOR_WHITE = (255,255,255)
+
+#M
+MENU_OPTION = ('New Game 1P',
+               'New Game 2P - Cooperative',
+               'New Game 2P - Competitive',
+               'Score',
+               'Exit')
+
+# W
+WIN_WIDTH = 480
+WIN_HEIGHT = 270
--- code/Game.py
+++ code/Game.py
@@ -2,28 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()  # Inicializa o Pygame
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-        # Loop principal
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        running = False
-            #        pygame.quit()
-
-            #pygame.display.update()  # Atualiza a tela
-
-        # quit()
 
 if __name__ == "__main__":
     game = Game()
