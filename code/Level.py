7040afd622bfed6254fe6dff9496f246b6f94c4e
GabMatheus <gab_matheus@hotmail.com>
2025-03-17 20:22:32 -0300

    Changing Level and Menu

--- code/Level.py
+++ code/Level.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from code.Entity import Entity
+
 
 class Level:
-    def __init__(self):
-        self.window Surface = None
-        self.entity_list = None
-        self.name = None
+    def __init__(self ,window, name, game_mode):
+        self.window = window
+        self.entity_list: list[Entity] = []
+        self.game_mode = game_mode #modo de jogo
+        self.name = name
 
     def run(self, ):
         pass
--- code/Menu.py
+++ code/Menu.py
@@ -3,7 +3,7 @@
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
-from code.Const import WIN_WIDTH, COLOR_GOLD, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_GOLD, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -12,22 +12,40 @@ class Menu:
         self.surf = pygame.image.load('./asset/MenuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self, ):
+    def run(self,):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.wav')
         pygame.mixer_music.play(-1)
         while True:
+            #DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(35, 'Soldiers vs Zombies' , COLOR_GOLD , ((WIN_WIDTH/2) , 50))
+            self.menu_text(40, 'Soldiers vs Zombies' , COLOR_GOLD , ((WIN_WIDTH/2) , 50))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(15 , MENU_OPTION[i], COLOR_WHITE , ((WIN_WIDTH/2) , 185 + 12 * i))
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 185 + 15 * i))
+                else:
+                    self.menu_text(20 , MENU_OPTION[i], COLOR_WHITE , ((WIN_WIDTH/2) , 185 + 15 * i))
             pygame.display.flip()
 
+            #CHECK FOR ALL EVENTS
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit() #Close window
                     quit() #end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN: #DOWN KEY
+                        if menu_option < len(MENU_OPTION) -1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP: #UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN: #enter
+                        return MENU_OPTION[menu_option]
 
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
