feaea3ca305dd55be798325f73a371c07bd593bf
GabMatheus <gab_matheus@hotmail.com>
2025-03-17 21:31:21 -0300

    Creating Menu Interations, Creating ABS class Creating Factory Background Lvl 1: Paralax

--- code/Const.py
+++ code/Const.py
@@ -3,6 +3,16 @@ COLOR_GOLD = (212, 160, 28)
 COLOR_WHITE = (255,255,255)
 COLOR_YELLOW = (255,255,0)
 
+#E
+ENTITY_SPEED = {
+    'Level1Bg0' : 0,
+    'Level1Bg1' : 1,
+    'Level1Bg2' : 2,
+    'Level1Bg3' : 3,
+    'Level1Bg4' : 4,
+    'Level1Bg5' : 5
+}
+
 #M
 MENU_OPTION = ('New Game 1P',
                'New Game 2P - Cooperative',
--- code/Level.py
+++ code/Level.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import pygame.display
+
 from code.Entity import Entity
+from code.EntityFactory import EntityFactory
 
 
 class Level:
@@ -9,7 +12,14 @@ class Level:
         self.entity_list: list[Entity] = []
         self.game_mode = game_mode #modo de jogo
         self.name = name
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
-    def run(self, ):
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf , dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+
         pass
 
