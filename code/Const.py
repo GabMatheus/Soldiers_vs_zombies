054045a323ae387fce396c52cc9aa6d17fd0c24c
GabMatheus <gab_matheus@hotmail.com>
2025-03-25 20:57:24 -0300

    SQLite3 implementation for SCORE using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -8,6 +8,7 @@ C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
 C_ORANGE = (217, 119, 6)
 C_BLUE = (173, 216, 230)
+C_RED = (255, 20, 30)
 
 #E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -119,7 +120,7 @@ ENTITY_SCORE = {
 #M
 MENU_OPTION = ('New Game 1P',
                'New Game 2P - Cooperative',
-               'New Game 2P - Cooperative',
+               'New Game 2P - Competitive',
                'Score',
                'Exit')
 
@@ -145,3 +146,20 @@ TIMEOUT_LEVEL = 25000 #25s
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+#S-2
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290)
+        }
